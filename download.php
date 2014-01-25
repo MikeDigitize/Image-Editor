0cd4ea52991be5827dd2e2fb7441f765f64565fa
Michael Chadwick <chad_01@hotmail.co.uk>
2014-01-25 20:19:27 +0000

    minor edits

---
+++ download.php
@@ -0,0 +1,24 @@
+<?php
+
+	// get file name to download
+	$image = $_GET[ 'img' ];
+	if ( strpos( $image,'temp/' ) !== false ) {
+		
+		// set headers to allow download
+		if ( file_exists ($image) ) {
+			header( 'Content-Description: File Transfer' );
+			header( 'Content-Type: application/octet-stream' );
+			header( 'Content-Disposition: attachment; filename='.basename( $image ) );
+			header( 'Content-Transfer-Encoding: binary' );
+			header( 'Expires: 0' );
+			header( 'Cache-Control: must-revalidate' );
+			header( 'Pragma: public' );
+			header( 'Content-Length: ' . filesize( $image ) );
+			ob_clean();
+			flush();
+			readfile( $image );
+			exit;
+		}
+	}
+
+?>
